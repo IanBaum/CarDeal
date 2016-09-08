ccbbc91b97ea75018b7d45249bd3711c8196334a
Anand Angalig and Ian Baum <ian@>
2016-09-08 09:49:41 -0400

    move display logic to Twig files and initiate the SESSION

--- app/app.php
+++ app/app.php
@@ -2,50 +2,32 @@
     require_once __DIR__."/../vendor/autoload.php";
     require_once __DIR__."/../src/car_form.php";
 
+    session_start();
+    if (empty($_SESSION['list_of_cars'])) {
+      $_SESSION['list_of_cars'] = array();
+    };
+
     $app = new Silex\Application();
-    $app->get("/", function() {
-        return "
-            <!DOCTYPE html>
-            <html>
-              <head>
-                <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css'>
-                <title>BUY OUR CARS</title>
-              </head>
-              <body>
-                <div class='container'>
-                  <h1>Buy a car!</h1>
-                  <form action='/view_cars'>
-                    <div class='form-group'>
-                      <label for='price'>Maximum Price:</label>
-                      <input id='price' name='price' class='form-control' type='number>'
-                    </div>
-                    <div class='form-group'>
-                      <label for='miles'>Maximum Miles:</label>
-                      <input id='miles' name='miles' class='form-control' type='number>'
-                    </div>
-                    <button type='submit class=btn-success'>Submit</button>
-                  </form>
-                </div>
-              </body>
-            </html>
-        ";
+
+    $app->register(new Silex\Provider\TwigServiceProvider(), array(
+      'twig.path' => __DIR__.'/../views'
+    ));
+
+    $app->get("/", function() use($app) {
+        return $app['twig']->render('cars.html.twig');
     });
+
 // NOTE: this is similar to frontsies
-    $app->get("/view_cars", function(){
+    $app->get("/view_cars", function() use($app){
       $maxPrice = $_GET['price'];
       $maxMiles = $_GET['miles'];
       $float_price = (float) $maxPrice;
       $float_miles = (float) $maxMiles;
-      $cars = [];
       $tesla = new Car("2016 Tesla",10000,12000000000000,"/img/tesla.jpeg");
       $tonka = new Car("2016 Tonka",25000,12324,"/img/tonka.jpg");
       $bat = new Car("2016 Batmobile",50000,14111,"/img/bat.jpg");
       $boat = new Car("It's a boat",100000,2,"/img/boat.jpg");
-      array_push($cars, $tesla);
-      array_push($cars, $tonka);
-      array_push($cars, $bat);
-      array_push($cars, $boat);
-      $inventory = "";
+      $cars = [$tesla, $tonka, $bat, $boat];
 
         $cars_matching_search = [];
         foreach ($cars as $car) {
@@ -53,20 +35,7 @@
                 array_push($cars_matching_search, $car);
             }
         }
-
-        if (empty($cars_matching_search)) {
-            return "<p>We have no cars meeting your stringent criteria.</p>
-            <a href='/../'>Go Back</a>";
-        }
-      foreach ($cars_matching_search as $car) {
-          $inventory .= "<li>" . $car->getModel() . "</li>";
-          $inventory .= "<ul>";
-          $inventory .= "<li>$" . $car->getPrice() . "</li>";
-          $inventory .= "<li>Miles: " . $car->getMiles() . "</li>";
-          $inventory .= "<li><img src='" . $car->getPicture() . "'></li>";
-          $inventory .= "</ul>";
-      }
-      return $inventory;
+      return $app['twig']->render('viewcars.html.twig', array('cars'=>$cars_matching_search));
     });
 
     return $app;
--- src/car_form.php
+++ src/car_form.php
@@ -37,4 +37,16 @@ class Car{
     function getPicture(){
       return $this->picture;
     }
+
+    function save() {
+      array_push($_SESSION['list_of_cars'], $this);
+    }
+
+    static function getAll() {
+      return $_SESSION['list_of_cars'];
+    }
+
+    static function deleteAll() {
+      $_SESSION['list_of_cars'] = array();
+    }
 }
